6cf494a60fdd5718171fec61ad7fbfc6f851838f
harvy666 <harvyhun@gmail.com>
2023-02-01 17:33:15 +0100

    first commit

---
+++ src/main/java/com/example/ConferenceRoom/ConferenceRoomApplication.java
@@ -0,0 +1,13 @@
+package com.example.ConferenceRoom;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConferenceRoomApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConferenceRoomApplication.class, args);
+	}
+
+}
