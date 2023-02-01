9c0da79aec90d9ab72cc94615ec2420b14e95e39
harvy666 <harvyhun@gmail.com>
2023-02-01 22:51:27 +0100

    add create table sql and Flyway

--- src/main/java/com/example/ConferenceRoom/ConferenceRoomApplication.java
+++ src/main/java/com/example/ConferenceRoom/ConferenceRoomApplication.java
@@ -1,5 +1,6 @@
 package com.example.ConferenceRoom;
 
+import org.flywaydb.core.Flyway;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -7,6 +8,13 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class ConferenceRoomApplication {
 
 	public static void main(String[] args) {
+		Flyway flyway = Flyway.configure()
+				.dataSource("jdbc:h2:mem:conference", "sa", null)
+				.locations("db/migration")
+				.load();
+
+		flyway.migrate();
+
 		SpringApplication.run(ConferenceRoomApplication.class, args);
 	}
 
