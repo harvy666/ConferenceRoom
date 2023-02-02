28c8b968a39c515290cad839edd558838c4dba96
harvy666 <harvyhun@gmail.com>
2023-02-02 12:52:47 +0100

    NOT working, add modelAndView to controller

--- src/main/java/com/example/ConferenceRoom/controller/CheckBoxDataController.java
+++ src/main/java/com/example/ConferenceRoom/controller/CheckBoxDataController.java
@@ -1,9 +1,13 @@
 package com.example.ConferenceRoom.controller;
 
+import com.example.ConferenceRoom.entity.CheckBoxData;
 import com.example.ConferenceRoom.service.CheckBoxDataService;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+import java.util.List;
 
 @Controller
 @RequestMapping("/")
@@ -15,9 +19,20 @@ public class CheckBoxDataController {
     public CheckBoxDataController(CheckBoxDataService checkBoxDataService) {
         this.checkBoxDataService = checkBoxDataService;
     }
+//
+//    @GetMapping()
+//    public String getIndex() {
+//        return "index";
+//    }
+
+//TODO maybe good solution?
 
     @GetMapping()
-    public String getIndex() {
-        return "index";
+    public ModelAndView getData() {
+        List<CheckBoxData> data = checkBoxDataService.getAllCheckBoxData();
+        ModelAndView mav = new ModelAndView();
+        mav.addObject("data", data);
+        mav.setViewName("index");
+        return mav;
     }
 }
